6122ce0e88c77a4c356e32d2d9d51ce98198b6fd
Alex <kavanokami@gmail.com>
2020-03-24 20:33:36 +0400

    feat: add CsvFileReader

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
 
-const matches = fs
-  .readFileSync('football.csv', { encoding: 'utf-8' })
-  .split('\n')
-  .map((row: string): string[] => row.split(','));
+const reader = new CsvFileReader('football.csv');
+reader.read();
 
 let manUnitedWins = 0;
 
@@ -13,7 +11,7 @@ enum MatchResult {
   Draw = 'D'
 }
 
-for (let match of matches) {
+for (let match of reader.data) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
