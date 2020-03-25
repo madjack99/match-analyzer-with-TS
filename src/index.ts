d936a93e92ea9149822c647edf1ab365ced9e636
Alex <kavanokami@gmail.com>
2020-03-25 17:21:34 +0400

    feat: add static method to Summary

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,6 @@ const csvFileReader = new CsvFileReader('football.csv');
 const reader = new MatchReader(csvFileReader);
 reader.load();
 
-const summary = new Summary(new WinsAnalysis('Newcastle'), new HtmlReport());
+// const summary = new Summary(new WinsAnalysis('Newcastle'), new HtmlReport());
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 summary.buildAndPrintReport(reader.matches);
