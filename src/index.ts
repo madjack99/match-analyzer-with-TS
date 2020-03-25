25c110959d7b6b71f9f6e1888be4c7e81baa4977
Alex <kavanokami@gmail.com>
2020-03-25 14:26:32 +0400

    refactor: to use interface and two classes objects

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import { MatchReader } from './MatchReader';
+import { CsvFileReader } from './CsvFileReader';
 import { MatchResult } from './MatchResult';
+import { MatchReader } from './MatchReader';
 
-const reader = new MatchReader('football.csv');
-reader.read();
-
-console.log(reader.data[0][0]);
+const csvFileReader = new CsvFileReader('football.csv');
+const reader = new MatchReader(csvFileReader);
+reader.load();
 
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+for (let match of reader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
