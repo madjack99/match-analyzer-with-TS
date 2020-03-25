e151e81d9ee6a854bb4375ef290635c76328fc99
Alex <kavanokami@gmail.com>
2020-03-25 17:10:07 +0400

    feat: add HtmlReport class

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
 import { Summary } from './Summary';
 import { WinsAnalysis } from './analyzers/WinsAnalysis';
-import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { HtmlReport } from './reportTargets/HtmlReport';
 
 const csvFileReader = new CsvFileReader('football.csv');
 const reader = new MatchReader(csvFileReader);
 reader.load();
 
-const summary = new Summary(new WinsAnalysis('Newcastle'), new ConsoleReport());
+const summary = new Summary(new WinsAnalysis('Newcastle'), new HtmlReport());
 summary.buildAndPrintReport(reader.matches);
